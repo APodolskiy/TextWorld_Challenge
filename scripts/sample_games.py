4c536dd3fca428141eca3aa05136e96f0be2f6e9
dima <dimich3d@ya.ru>
2019-02-17 11:35:22 +0300

    Script for subsampling games

---
+++ scripts/sample_games.py
@@ -0,0 +1,29 @@
+"""Folder with training games is too big, for debugging purposes it's useful to sample just some of them"""
+import shutil
+from pathlib import Path
+
+import fire
+import numpy
+
+
+def sample_games(files_dir, num_games):
+    numpy.random.seed(0)
+    files = sorted(Path(files_dir).iterdir())
+    # each game is defined by .json, .z8, .ulx files
+    total_games = len(files) // 3
+    sampled_games = numpy.random.choice(total_games, num_games, replace=False)
+    indices = numpy.sort(sampled_games)
+    sampled_files = []
+    for idx in indices:
+        sampled_files.extend(files[idx * 3: idx * 3 + 3])
+    return sampled_files
+
+
+def main(files_dir="train", num_games=10, saving_dir="train_sample"):
+    res = sample_games(files_dir, num_games)
+    for gamefile in res:
+        shutil.copy(str(gamefile), saving_dir)
+    print(res)
+
+if __name__ == '__main__':
+    fire.Fire(main)
