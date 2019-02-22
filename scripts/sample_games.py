6416dd6b28962300268deed98026044f0959c81b
a.podolskiy <a.podolskiy@partner.samsung.com>
2019-02-22 12:34:28 +0300

    Add documentation

--- scripts/sample_games.py
+++ scripts/sample_games.py
@@ -44,6 +44,14 @@ def sample_games_by_level(files_dir: str,
                           level_max: int,
                           level_min: int = 0,
                           max_games: Optional[int] = None) -> List[Path]:
+    """
+    Sample games by level
+    :param files_dir: path to the directory with game files
+    :param level_max: maximum game level
+    :param level_min: minimum game level
+    :param max_games: maximum number of games to sample
+    :return: list of `pathlib.Path` objects related to the game files
+    """
     assert level_max > 0
     assert level_min >= 0
     assert (max_games is None) or (max_games > 0)
@@ -53,13 +61,13 @@ def sample_games_by_level(files_dir: str,
 
     files = defaultdict(list)
     games_cnt = 0
-    for file in Path(files_dir).iterdir():
-        if level_min <= count_skills(file.stem) <= level_max:
-            files[file.stem].append(file)
+    for game_file in Path(files_dir).iterdir():
+        if level_min <= count_skills(game_file.stem) <= level_max:
+            files[game_file.stem].append(game_file)
             games_cnt += 1
     file_groups = list(files.values())
     max_games = max_games or games_cnt
-    return [file for file_group in file_groups[:max_games] for file in file_group]
+    return [game_file for file_group in file_groups[:max_games] for game_file in file_group]
 
 
 def sample_games_by_skill(files_dir: str,
@@ -67,35 +75,58 @@ def sample_games_by_skill(files_dir: str,
                           skill_cnt_max: int,
                           skill_cnt_min: int = 0,
                           max_games: Optional[int] = None) -> List[Path]:
+    """
+    Sample games by skill level from spcified directory.
+    :param files_dir: path to the directory with games
+    :param skill: skill name
+    :param skill_cnt_max: maximum skill level
+    :param skill_cnt_min: minimum skill level
+    :param max_games: maximum number of games to sample
+    :return: list of `pathlib.Path` objects related to the game files.
+    """
     assert skill_cnt_max > 0
     assert skill_cnt_min >= 0
     assert (max_games is None) or (max_games > 0)
 
     files = defaultdict(list)
     games_cnt = 0
-    for file in Path(files_dir).iterdir():
-        skills = acquire_skills(file.stem)
+    for game_file in Path(files_dir).iterdir():
+        skills = acquire_skills(game_file.stem)
         if skill in skills and skill_cnt_min <= skills[skill] <= skill_cnt_max:
-            files[file.stem].append(file)
+            files[game_file.stem].append(game_file)
             games_cnt += 1
     file_groups = list(files.values())
     max_games = max_games or games_cnt
-    return [file for file_group in file_groups[:max_games] for file in file_group]
+    return [game_file for file_group in file_groups[:max_games] for game_file in file_group]
 
 
-def count_skills(game_name):
+def count_skills(game_name: str) -> int:
+    """
+    Count skills needed for the game.
+    :param game_name: game name
+    :return: number of skill in the game
+    """
     skills = acquire_skills(game_name)
     return sum(skills.values())
 
 
 def acquire_skills(game_name: str) -> Dict[str, int]:
+    """
+    Acquire needed skills from the games name.
+    :param game_name: game name
+    :return: dict with skill as a key and skill count as a value
+    """
     skill_field = game_name.split("-")[2]
     skills = skill_field.split("+")
-    # TODO: all skills are differnt in the naming of files?
     return {truncate_skill(skill): get_skill_count(skill) for skill in skills}
 
 
 def get_skill_count(skill: str) -> int:
+    """
+    Count the skill
+    :param skill: raw skill name
+    :return: skill's count
+    """
     num = FIND_NUMBER.findall(skill)
     if len(num) == 0:
         return 1
@@ -103,6 +134,18 @@ def get_skill_count(skill: str) -> int:
 
 
 def truncate_skill(skill: str) -> str:
+    """
+    Truncate skill's name.
+
+    :param skill: raw skill name
+    :return: truncated skill name
+
+    -------
+    Example:
+    'goal6' will be transformed into 'goal'
+    'goal12' will be transformed into 'goal'
+    'goal' will be transformed into 'goal'
+    """
     num = FIND_NUMBER.findall(skill)
     if len(num) == 0:
         return skill
@@ -110,6 +153,9 @@ def truncate_skill(skill: str) -> str:
 
 
 class SampleGames:
+    """
+    Script's entry point.
+    """
     def __init__(self,
                  files_dir: str = "games/train",
                  saving_dir: str = "games/train_sample/",
@@ -119,29 +165,54 @@ class SampleGames:
         self.force = force
         self._create_saving_dir()
 
-    def _create_saving_dir(self):
+    def _create_saving_dir(self) -> None:
+        """
+        Create directory where game files will be stored.
+        """
         saving_dir = Path(self.saving_dir)
         if saving_dir.exists() and self.force:
             shutil.rmtree(str(saving_dir))
         saving_dir.mkdir(parents=True, exist_ok=False)
 
-    def _copy_games(self, games: List[Path]):
+    def _copy_games(self, games: List[Path]) -> None:
+        """
+        Copy games into directory
+        :param games: list of `pathlib.Path` related to the game files
+        """
         for gamefile in games:
             shutil.copy(str(gamefile), str(self.saving_dir))
         pprint(games)
 
-    def random(self, num_games: int = 10):
+    def random(self, num_games: int = 10) -> None:
+        """
+        Sample random games
+        :param num_games: number of games to sample
+        :return:
+        """
         games = sample_random_games(self.files_dir, num_games)
         self._copy_games(games)
 
-    def level(self, level_max: int, level_min: int = 0, max_games: Optional[int] = None):
+    def level(self, level_max: int, level_min: int = 0, max_games: Optional[int] = None) -> None:
+        """
+        Sample games by level
+        :param level_max: maximum level
+        :param level_min: minimum level
+        :param max_games: maximum number of games to sample
+        """
         games = sample_games_by_level(files_dir=self.files_dir,
                                       level_max=level_max,
                                       level_min=level_min,
                                       max_games=max_games)
         self._copy_games(games)
 
-    def skill(self, skill: str, skill_cnt_max: int, skill_cnt_min: int = 0, max_games: Optional[int] = None):
+    def skill(self, skill: str, skill_cnt_max: int, skill_cnt_min: int = 0, max_games: Optional[int] = None) -> None:
+        """
+        Sample games by specific skill
+        :param skill: skill name
+        :param skill_cnt_max: maximum level of skill
+        :param skill_cnt_min: minimum level of skill
+        :param max_games: maximum number of games to sample
+        """
         games = sample_games_by_skill(files_dir=self.files_dir,
                                       skill=skill,
                                       skill_cnt_max=skill_cnt_max,
