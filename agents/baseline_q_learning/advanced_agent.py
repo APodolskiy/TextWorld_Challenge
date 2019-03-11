e713e32de66f6ad9f24291c7d9aae625beff51b1
Dima Lipin <dimich3d@ya.ru>
2019-03-11 15:43:25 +0300

    Start S NULYAAAAAAAAAAAAA

--- agents/baseline_q_learning/advanced_agent.py
+++ agents/baseline_q_learning/advanced_agent.py
@@ -12,6 +12,27 @@ from agents.utils.eps_scheduler import EpsScheduler
 from agents.utils.params import Params
 
 
+class QNet(Module):
+    def __init__(self, config: Params):
+        super().__init__()
+
+        self.hidden_size = config.pop("hidden_size")
+        self.embedding_size = config.pop("embedding_size")
+
+        self.obs_to_hidden = nn.Linear(self.embedding_size, self.hidden_size)
+        self.actions_to_hidden = nn.Linear(self.embedding_size, self.hidden_size)
+        self.hidden_to_scores = nn.Linear(self.hidden_size, 1)
+
+        self.lrelu = nn.LeakyReLU(0.2)
+
+    def forward(self, observation, actions):
+        obs = self.obs_to_hidden(observation)
+        actions = self.actions_to_hidden(actions)
+        final_state = self.lrelu(obs * actions)
+        q_values = self.hidden_to_scores(final_state)
+        return q_values
+
+
 class BaseQlearningAgent:
     """ Q-learning agent that requires all available information and therefore receives maximum
     penalty
@@ -22,6 +43,14 @@ class BaseQlearningAgent:
         self._epsiode_has_started = False
         self.device = config.pop("device")
         self.max_steps_per_episode = config.pop("max_steps_per_episode")
+
+        self.bert = (
+            BertModel.from_pretrained("bert-base-uncased").to(self.device).eval()
+        )
+        self.tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+
+        self.qnet = QNet(config.pop("network"))
+        self.eps_scheduler = EpsScheduler(config.pop("epsilon"))
 
         self.current_step = 0
 
@@ -132,11 +161,89 @@ class BaseQlearningAgent:
 
     def act(
         self,
-        obs: List[str],
+        obs: str,
         scores: List[int],
         dones: List[bool],
         infos: Dict[str, List[Any]],
     ):
-        batch_admissible_commands = infos["admissible_commands"]
-        command = [random.choice(adm_com) for adm_com in batch_admissible_commands]
+        """
+        Acts upon the current list of observations.
+
+        One text command must be returned for each observation.
+
+        Arguments:
+            obs: Previous command's feedback for each game.
+            scores: The score obtained so far for each game.
+            dones: Whether a game is finished.
+            infos: Additional information for each game.
+
+        Returns:
+            Text commands to be performed (one per observation).
+            If episode had ended (e.g. `all(dones)`), the returned
+            value is ignored.
+
+        Notes:
+            Commands returned for games marked as `done` have no effect.
+            The states for finished games are simply copy over until all
+            games are done.
+        """
+        # if all(dones):
+        #     self._end_episode(obs, scores, infos)
+        #     return  # Nothing to return.
+        #
+        # if not self._epsiode_has_started:
+        #     self._start_episode(obs, infos)
+        # TODO: no batching currecntly
+        admissible_commands = infos["admissible_commands"]
+
+        if random.random() < self.eps_scheduler.eps(self.current_step):
+            command = random.choice(admissible_commands)
+        else:
+            state_repr = self.embed_observation(obs, infos)
+            actions_repr = self.embed_actions(admissible_commands)
+            q_values = self.qnet(state_repr, torch.cat(actions_repr, dim=0))
+            max_q_val, idx_max_q_val = q_values.max()
+            command = admissible_commands[idx_max_q_val]
+        self.current_step += 1
         return command
+        # raise NotImplementedError()
+        # [Insert your code here to obtain the commands.]
+        # return ["wait"] * len(obs)  # No-op
+
+    def embed_observation(self, obs: str, infos: Dict):
+        # TODO: change sep to smth other?
+        state_description = (
+            "[CLS] "
+            + "[SEP]".join([obs, infos["description"], infos["inventory"]])
+            + " [SEP]"
+        )
+
+        tokenized_state_description = self.tokenizer.tokenize(state_description)
+        cleaned_tokenized_state_decription = [
+            token
+            for token in tokenized_state_description
+            if token not in {"$", "|", "", "_", "\\", "/"}
+        ]
+        indexed_state_description = self.tokenizer.convert_tokens_to_ids(
+            cleaned_tokenized_state_decription
+        )
+        indexed_state_description = torch.tensor(
+            [indexed_state_description], device=self.device
+        )
+        # TODO: fine-tune?
+        with torch.no_grad():
+            _, state_repr = self.bert(indexed_state_description)
+        return state_repr
+
+    def embed_actions(self, actions):
+        embedded_actions = []
+        with torch.no_grad():
+            for action in actions:
+                tokenzed_action = self.tokenizer.tokenize(f"[CLS] {action} [SEP]")
+                action_indices = torch.tensor(
+                    [self.tokenizer.convert_tokens_to_ids(tokenzed_action)],
+                    device=self.device,
+                )
+                _, action_embedding = self.bert(action_indices)
+                embedded_actions.append(action_embedding)
+        return embedded_actions
