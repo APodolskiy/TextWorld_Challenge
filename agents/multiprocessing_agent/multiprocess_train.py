ef2c10ef36f9c5e66095dc4499517c0e4da7eed4
Dima Lipin <dimich3d@ya.ru>
2019-03-13 18:38:35 +0300

    pass path to config in train script

--- agents/multiprocessing_agent/multiprocess_train.py
+++ agents/multiprocessing_agent/multiprocess_train.py
@@ -1,23 +1,19 @@
 import argparse
 import logging
 from multiprocessing import Queue
-
 from pathlib import Path
 from queue import Empty
-from time import sleep
+from shutil import rmtree
 
-import gym
-import textworld
-from tqdm import tqdm
 import torch.multiprocessing as mp
 
 from agents.multiprocessing_agent.collecting import collect_experience
 from agents.multiprocessing_agent.custom_agent import QNet
-from agents.multiprocessing_agent.custom_agent import BaseQlearningAgent
 from agents.multiprocessing_agent.learning import learn
 from agents.utils.params import Params
 from agents.utils.replay import ExperienceReplay
-from test_submission import _validate_requested_infos
+
+logging.basicConfig(level=logging.INFO)
 
 
 def clear(q):
@@ -28,79 +24,12 @@ def clear(q):
         pass
 
 
-# def collect_experience(game_files, policy_net, target_net, buffer: Queue, params):
-#
-#     # TODO: get from policy_net
-#
-#     logging.basicConfig(level=logging.INFO)
-#     train_params = params.pop("training")
-#
-#     actor = BaseQlearningAgent(
-#         net=target_net, experience_replay_buffer=buffer, params=params.pop("agent")
-#     )
-#
-#     requested_infos = actor.select_additional_infos()
-#     _validate_requested_infos(requested_infos)
-#
-#     env_id = textworld.gym.register_games(
-#         game_files,
-#         requested_infos,
-#         max_episode_steps=actor.max_steps_per_episode,
-#         name="training",
-#     )
-#     batch_size = 2
-#     env_id = textworld.gym.make_batch(env_id, batch_size=batch_size, parallel=True)
-#     env = gym.make(env_id)
-#
-#     for epoch_no in range(1, train_params.pop("n_epochs") + 1):
-#         stats = {"scores": [], "steps": []}
-#         for _ in tqdm(range(len(game_files))):
-#             obs, infos = env.reset()
-#             actor.train()
-#
-#             dones = [False for _ in range(batch_size)]
-#             steps = [0 for _ in range(batch_size)]
-#             scores = [0 for _ in range(batch_size)]
-#             while not all(dones):
-#                 # Increase step counts.
-#                 steps = [step + int(not done) for step, done in zip(steps, dones)]
-#                 command = actor.act(obs, scores, dones, infos)
-#                 obs, scores, dones, infos = env.step(command)
-#
-#         score = sum(stats["scores"])
-#         steps = sum(stats["steps"])
-#         print(f"Epoch: {epoch_no:3d} | {score:2.1f} pts | {steps:4.1f} steps")
-#     print("Returning")
-#     return
-#
-#
-# def train_net(net, queue, buffer: ExperienceReplay):
-#
-#     # TODO: actually train
-#
-#     for _ in range(10):
-#         sleep(1)
-#         for _ in range(4):
-#             sample = queue.get()
-#             buffer.push(sample)
-#
-#         try:
-#             batch = buffer.sample(10)
-#             print(batch)
-#         except ValueError as e:
-#             print(e)
-#     clear(queue)
-
-logging.basicConfig(level=logging.INFO)
-
 if __name__ == "__main__":
+    mp.set_start_method("spawn")
     parser = argparse.ArgumentParser(description="Train baseline Q-learning agent.")
     parser.add_argument(
         "games", metavar="game", type=str, help="path to the folder with games"
     )
-
-    mp.set_start_method("spawn")
-
     args = parser.parse_args()
     train_dir = Path(args.games)
     games = [str(f) for f in train_dir.iterdir() if f.is_file() and f.suffix == ".ulx"]
@@ -117,9 +46,13 @@ if __name__ == "__main__":
     target_net.load_state_dict(policy_net.state_dict())
     target_net.share_memory()
 
-    processes = []
+    # TODO: change this
+    log_dir = "runs/"
+    rmtree(log_dir)
+
     queue = Queue(maxsize=100_000)
     replay = ExperienceReplay()
+    processes = []
     collecting_process = mp.Process(
         target=collect_experience,
         kwargs={
@@ -129,6 +62,7 @@ if __name__ == "__main__":
             "eps_scheduler_params": params.pop("epsilon"),
             "target_net": target_net,
             "policy_net": policy_net,
+            "log_dir": log_dir
         },
     )
 
@@ -142,6 +76,7 @@ if __name__ == "__main__":
             "replay_buffer": exp_replay,
             "queue": queue,
             "params": train_params,
+            "log_dir": log_dir
         },
     )
 
