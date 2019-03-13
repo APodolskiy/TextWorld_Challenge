60bfcfbbb0d7042be5a790361f2b8c3281523eee
Dima Lipin <dimich3d@ya.ru>
2019-03-13 12:57:37 +0300

    use parallel envs flag in actor

--- agents/multiprocessing_agent/collecting.py
+++ agents/multiprocessing_agent/collecting.py
@@ -5,8 +5,7 @@ from agents.multiprocessing_agent.custom_agent import BaseQlearningAgent
 from test_submission import _validate_requested_infos
 
 
-def collect_experience(game_files, buffer, params, target_net):
-    train_params = params.pop("training")
+def collect_experience(game_files, buffer, train_params, target_net):
     actor = BaseQlearningAgent(
         net=target_net, experience_replay_buffer=buffer, config=train_params
     )
@@ -20,7 +19,11 @@ def collect_experience(game_files, buffer, params, target_net):
         name="training",
     )
     batch_size = train_params.pop("n_parallel_envs")
-    env_id = textworld.gym.make_batch(env_id, batch_size=batch_size, parallel=False)
+    env_id = textworld.gym.make_batch(
+        env_id,
+        batch_size=batch_size,
+        parallel=train_params.pop("use_seperate_process_envs"),
+    )
     env = gym.make(env_id)
 
     for epoch_no in range(1, train_params.pop("n_epochs") + 1):
@@ -30,7 +33,6 @@ def collect_experience(game_files, buffer, params, target_net):
 
             dones = [False for _ in range(batch_size)]
             steps = [0 for _ in range(batch_size)]
-            cumulative_rewards = None
             prev_cumulative_rewards = [0 for _ in range(batch_size)]
             rewards = prev_cumulative_rewards
 
@@ -44,4 +46,3 @@ def collect_experience(game_files, buffer, params, target_net):
                     for c_r, p_c_r in zip(cumulative_rewards, prev_cumulative_rewards)
                 ]
                 prev_cumulative_rewards = cumulative_rewards
-            print(cumulative_rewards)
