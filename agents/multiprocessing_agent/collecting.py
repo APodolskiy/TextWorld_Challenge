3f060b614e61cbe36abf30f97d2454865674843a
Dima Lipin <dimich3d@ya.ru>
2019-03-13 12:50:48 +0300

    refactor collecting function

--- agents/multiprocessing_agent/collecting.py
+++ agents/multiprocessing_agent/collecting.py
@@ -1,22 +1,15 @@
-import logging
-from multiprocessing.queues import Queue
-
 import gym
 import textworld
 from tqdm import tqdm
-
-from agents.baseline_q_learning.custom_agent import BaseQlearningAgent
+from agents.multiprocessing_agent.custom_agent import BaseQlearningAgent
 from test_submission import _validate_requested_infos
 
 
-def collect_experience(game_files, main_net, target_net, buffer: Queue, params):
-    logging.basicConfig(level=logging.INFO)
+def collect_experience(game_files, buffer, params, target_net):
     train_params = params.pop("training")
-
     actor = BaseQlearningAgent(
-        net=target_net, experience_replay_buffer=buffer, config=params.pop("agent")
+        net=target_net, experience_replay_buffer=buffer, config=train_params
     )
-
     requested_infos = actor.select_additional_infos()
     _validate_requested_infos(requested_infos)
 
@@ -26,27 +19,29 @@ def collect_experience(game_files, main_net, target_net, buffer: Queue, params):
         max_episode_steps=actor.max_steps_per_episode,
         name="training",
     )
-    batch_size = 2
-    env_id = textworld.gym.make_batch(env_id, batch_size=batch_size, parallel=True)
+    batch_size = train_params.pop("n_parallel_envs")
+    env_id = textworld.gym.make_batch(env_id, batch_size=batch_size, parallel=False)
     env = gym.make(env_id)
 
     for epoch_no in range(1, train_params.pop("n_epochs") + 1):
-        stats = {"scores": [], "steps": []}
         for _ in tqdm(range(len(game_files))):
             obs, infos = env.reset()
             actor.train()
 
             dones = [False for _ in range(batch_size)]
             steps = [0 for _ in range(batch_size)]
-            scores = [0 for _ in range(batch_size)]
-            while not all(dones):
-                # Increase step counts.
-                steps = [step + int(not done) for step, done in zip(steps, dones)]
-                command = actor.act(obs, scores, dones, infos)
-                obs, scores, dones, infos = env.step(command)
+            cumulative_rewards = None
+            prev_cumulative_rewards = [0 for _ in range(batch_size)]
+            rewards = prev_cumulative_rewards
 
-        score = sum(stats["scores"])
-        steps = sum(stats["steps"])
-        print(f"Epoch: {epoch_no:3d} | {score:2.1f} pts | {steps:4.1f} steps")
-    print("Returning")
-    return
+            while not all(dones):
+                steps = [step + int(not done) for step, done in zip(steps, dones)]
+                command = actor.act(obs, rewards, dones, infos)
+                obs, cumulative_rewards, dones, infos = env.step(command)
+
+                rewards = [
+                    c_r - p_c_r
+                    for c_r, p_c_r in zip(cumulative_rewards, prev_cumulative_rewards)
+                ]
+                prev_cumulative_rewards = cumulative_rewards
+            print(cumulative_rewards)
