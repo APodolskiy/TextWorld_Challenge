2b07df3e115e51ea0671b8010fea37e5d824e26a
Dima Lipin <dimich3d@ya.ru>
2019-03-14 17:07:39 +0300

    change binaryexp replay

--- agents/utils/replay.py
+++ agents/utils/replay.py
@@ -1,3 +1,5 @@
+from logging import warning
+
 from overrides import overrides
 import random
 from typing import NamedTuple, List
@@ -7,7 +9,8 @@ class AbstractReplayMemory:
     """
     Abstract class that describes basic API for Replay Memory
     """
-    def __init__(self, capacity: int = 100000):
+
+    def __init__(self, capacity: int = 100_000):
         self.capacity = capacity
 
     def push(self, transition: NamedTuple):
@@ -30,7 +33,8 @@ class ExperienceReplay(AbstractReplayMemory):
     """
     Simple Experience Replay memory.
     """
-    def __init__(self, capacity: int = 100000):
+
+    def __init__(self, capacity: int = 100_000):
         super(ExperienceReplay, self).__init__(capacity=capacity)
         self.buffer = []
         self.position = 0
@@ -49,12 +53,15 @@ class ExperienceReplay(AbstractReplayMemory):
     @overrides
     def sample(self, batch_size: int) -> List[NamedTuple]:
         if len(self.buffer) < batch_size:
-            raise ValueError(f"Can't sample batch of size {batch_size} from the buffer!\n"
-                             f"There are only {len(self.buffer)} elements in the buffer.")
+            raise ValueError(
+                f"Can't sample batch of size {batch_size} from the buffer!\n"
+                f"There are only {len(self.buffer)} elements in the buffer."
+            )
         return random.sample(self.buffer, batch_size)
 
     def __len__(self):
         return len(self.buffer)
+
 
 # TODO: create sum tree data structure
 # TODO: continue creation of Prioritized Experience replay
@@ -63,8 +70,14 @@ class PrioritizedReplayMemory(AbstractReplayMemory):
     Experience Replay memory that stores transitions with priorities and samples
     elements according to these priorities.
     """
-    def __init__(self, capacity: int = 100000, alpha: float = 0.6,
-                 beta_init: float = 0.4, beta_anneal_steps: int = 10000):
+
+    def __init__(
+        self,
+        capacity: int = 100_000,
+        alpha: float = 0.6,
+        beta_init: float = 0.4,
+        beta_anneal_steps: int = 10000,
+    ):
         super(PrioritizedReplayMemory, self).__init__(capacity=capacity)
         self.alpha_smoothing = alpha
         self.buffer = []
@@ -90,7 +103,7 @@ class PrioritizedReplayMemory(AbstractReplayMemory):
 
 
 class BinaryPrioritizeReplayMemory(AbstractReplayMemory):
-    def __init__(self, capacity: int = 100000, priority_fraction: float = 0.0):
+    def __init__(self, capacity: int = 100_000, priority_fraction: float = 0.0):
         super(BinaryPrioritizeReplayMemory, self).__init__(capacity=capacity)
         self.prior_buffer = []
         self.prior_position = 0
@@ -118,22 +131,33 @@ class BinaryPrioritizeReplayMemory(AbstractReplayMemory):
         if len(self.secondary_buffer) < self.secondary_capacity:
             self.secondary_buffer.append(None)
         self.secondary_buffer[self.secondary_position] = transition
-        self.secondary_position = (self.secondary_position + 1) % self.secondary_capacity
+        self.secondary_position = (
+            self.secondary_position + 1
+        ) % self.secondary_capacity
 
     def sample(self, batch_size: int) -> List[NamedTuple]:
-        if self.priority_fraction > 0.0:
-            if len(self.secondary_buffer) < batch_size:
-                raise ValueError(f"Can't sample batch of size {batch_size} from the buffer!\n"
-                                 f"There are only {len(self.secondary_buffer)} elements in the buffer.")
-            return random.sample(self.secondary_buffer, batch_size)
+        if self.priority_fraction == 0.0:
+            samples = random.sample(self.secondary_buffer, batch_size)
         else:
-            prior_size = min(int(batch_size * self.priority_fraction), len(self.prior_buffer))
+            prior_size = min(
+                int(batch_size * self.priority_fraction), len(self.prior_buffer)
+            )
             secondary_size = min(batch_size - prior_size, len(self.secondary_buffer))
             prior_samples = random.sample(self.prior_buffer, prior_size)
             secondary_samples = random.sample(self.secondary_buffer, secondary_size)
             samples = prior_samples + secondary_samples
-            random.shuffle(samples)
-            return samples
+            # random.shuffle(samples)
+            if len(samples) != batch_size:
+                warning(
+                    f"Not enough elements in buffer: {len(prior_samples), len(secondary_samples)}"
+                )
+
+        # if len(self.secondary_buffer) < batch_size:
+        #     raise ValueError(
+        #         f"Can't sample batch of size {batch_size} from the buffer!\n"
+        #         f"There are only {len(self.secondary_buffer)} elements in the buffer."
+        #     )
+        return samples
 
     def __len__(self) -> int:
         return len(self.prior_buffer) + len(self.secondary_buffer)
