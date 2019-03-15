fb25ede9c3420a6fb86befc4909e85ca2f0e2687
Dima Lipin <dimich3d@ya.ru>
2019-03-15 20:02:51 +0300

    use simpler agent

--- agents/multiprocessing_agent/debug.py
+++ agents/multiprocessing_agent/debug.py
@@ -2,8 +2,11 @@ import logging
 from multiprocessing import Queue
 from pathlib import Path
 
+import spacy
+
 from agents.multiprocessing_agent.collecting import collect_experience
 from agents.multiprocessing_agent.learning import learn
+from agents.multiprocessing_agent.simple_net import SimpleNet, SimpleBowNet
 from agents.utils.params import Params
 from agents.multiprocessing_agent.custom_agent import QNet
 from agents.utils.replay import BinaryPrioritizeReplayMemory
@@ -18,23 +21,37 @@ if __name__ == "__main__":
     ][:1]
     params = Params.from_file("configs/debug_config.jsonnet")
     actor_device = params["training"].pop("actor_device")
-    my_net = QNet(params.get("network"), actor_device).to(actor_device)
+    my_net = SimpleBowNet(
+        device=actor_device, tokenizer=spacy.load("en_core_web_sm").tokenizer
+    ).to(actor_device)
+    learner_device = params["training"].pop("learner_device")
+    target_net = SimpleBowNet(
+        tokenizer=spacy.load("en_core_web_sm").tokenizer, device=learner_device
+    ).to(learner_device)
+    target_net.load_state_dict(my_net.state_dict())
     queue = Queue()
-    collect_experience(
-        buffer=queue,
-        train_params=params.get("training"),
-        eps_scheduler_params=params.pop("epsilon"),
-        game_files=games,
-        target_net=my_net,
-        policy_net=my_net,
-        log_dir=None
-    )
+
     replay_buffer = BinaryPrioritizeReplayMemory(capacity=500000, priority_fraction=0.5)
-    learn(
-        policy_net=my_net,
-        target_net=my_net,
-        replay_buffer=replay_buffer,
-        queue=queue,
-        params=params.pop("training"),
-        log_dir=None
-    )
+
+    train_params = params.pop("training")
+    eps_params = params.pop("epsilon")
+
+    for _ in range(1000):
+        collect_experience(
+            buffer=queue,
+            train_params=train_params.duplicate(),
+            eps_scheduler_params=eps_params.duplicate(),
+            game_files=games,
+            target_net=target_net,
+            policy_net=my_net,
+            log_dir="debug_runs/actor",
+        )
+
+        learn(
+            policy_net=my_net,
+            target_net=target_net,
+            replay_buffer=replay_buffer,
+            queue=queue,
+            params=train_params.duplicate(),
+            log_dir="debug_runs/learner_1e-4",
+        )
