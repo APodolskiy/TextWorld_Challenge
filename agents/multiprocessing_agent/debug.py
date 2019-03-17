4274f77044d50213f9ab3f1a49a718a7146e547a
dima <dimich3d@ya.ru>
2019-03-17 17:17:57 +0300

    update debug script

--- agents/multiprocessing_agent/debug.py
+++ agents/multiprocessing_agent/debug.py
@@ -1,4 +1,8 @@
 import logging
+from os.path import exists
+from shutil import rmtree
+
+import spacy
 from multiprocessing import Queue
 from pathlib import Path
 
@@ -21,11 +25,11 @@ if __name__ == "__main__":
     ][:1]
     params = Params.from_file("configs/debug_config.jsonnet")
     actor_device = params["training"].pop("actor_device")
-    my_net = SimpleBowNet(
+    my_net = SimpleNet(
         device=actor_device, tokenizer=spacy.load("en_core_web_sm").tokenizer
     ).to(actor_device)
     learner_device = params["training"].pop("learner_device")
-    target_net = SimpleBowNet(
+    target_net = SimpleNet(
         tokenizer=spacy.load("en_core_web_sm").tokenizer, device=learner_device
     ).to(learner_device)
     target_net.load_state_dict(my_net.state_dict())
@@ -35,6 +39,11 @@ if __name__ == "__main__":
 
     train_params = params.pop("training")
     eps_params = params.pop("epsilon")
+
+    log_dir = "debug_runs"
+    if exists(log_dir):
+        rmtree(log_dir)
+        Path(log_dir).mkdir(parents=True)
 
     for _ in range(1000):
         collect_experience(
@@ -53,5 +62,5 @@ if __name__ == "__main__":
             replay_buffer=replay_buffer,
             queue=queue,
             params=train_params.duplicate(),
-            log_dir="debug_runs/learner_1e-4",
+            log_dir="debug_runs/learner",
         )
