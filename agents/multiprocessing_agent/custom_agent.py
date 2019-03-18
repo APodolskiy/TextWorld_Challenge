0be01b6fb048aea2ba7fb5a53756b5995e217b66
Dima Lipin <dimich3d@ya.ru>
2019-03-18 18:27:57 +0300

    optimization: do not play finished games

--- agents/multiprocessing_agent/custom_agent.py
+++ agents/multiprocessing_agent/custom_agent.py
@@ -1,16 +1,11 @@
-import random
-from collections import namedtuple
+from numpy import random
+from collections import namedtuple, defaultdict
 from multiprocessing import Queue
-from typing import List, Dict, Any, Optional
+from typing import List, Dict, Any, Optional, Tuple
 
-import torch
-import torch.nn as nn
-from pytorch_pretrained_bert import BertModel, BertTokenizer
 from textworld import EnvInfos
-from torch.nn import Module, LayerNorm
-from torch.nn.functional import softmax
-from torch.nn.utils.rnn import pad_sequence
 
+from agents.multiprocessing_agent.bert_net import QNet
 from agents.utils.eps_scheduler import EpsScheduler
 from agents.utils.params import Params
 
@@ -18,99 +13,28 @@ State = namedtuple("State", ("description", "feedback", "inventory"))
 
 Transition = namedtuple(
     "Transition",
-    ("previous_state", "next_state", "action", "reward", "done", "allowed_actions"),
+    (
+        "previous_state",
+        "next_state",
+        "action",
+        "reward",
+        "exploration_bonus",
+        "done",
+        "allowed_actions",
+    ),
 )
 
 
-class QNet(Module):
-    def __init__(self, config: Params, device="cuda"):
-        super().__init__()
-
-        self.bert = BertModel.from_pretrained("bert-base-uncased").eval()
-        self.tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
-
-        self.hidden_size = config.get("hidden_size")
-        self.embedding_size = config.get("embedding_size")
-
-        # TODO: probably need recurrent nets here
-        self.obs_to_hidden = nn.Linear(self.embedding_size, self.hidden_size)
-        self.actions_to_hidden = nn.Linear(self.embedding_size, self.hidden_size)
-
-        self.hidden_to_hidden = nn.Linear(self.hidden_size, self.hidden_size // 2)
-
-        self.hidden_to_scores = nn.Linear(self.hidden_size // 2, 1)
-
-        self.state_layer_norm = LayerNorm(self.hidden_size)
-        self.action_layer_norm = LayerNorm(self.hidden_size)
-        self.hidden_layer_norm = LayerNorm(self.hidden_size // 2)
-
-        self.lrelu = nn.LeakyReLU(0.2)
-
-        self.device = device
-
-    # TODO: use receipt
-    def forward(self, state_batch, actions_batch):
-        observations, description, inventory = list(zip(*state_batch))
-        # TODO: memory issues, how to deal?
-        with torch.no_grad():
-            embedded_observations = self.embed_observations(
-                observations, description=description, inventory=inventory
-            )
-        q_values = []
-        for obs, act in zip(embedded_observations, actions_batch):
-
-            if isinstance(act, str):
-                act = [act]
-            with torch.no_grad():
-                embedded_actions = self.embed_actions(act)
-            obs = self.lrelu(self.obs_to_hidden(obs))
-            actions = self.lrelu(self.actions_to_hidden(embedded_actions))
-            final_state = self.state_layer_norm(obs) * self.action_layer_norm(actions)
-            new_hidden_size = self.hidden_layer_norm(
-                self.lrelu(self.hidden_to_hidden(final_state))
-            )
-            q_values.append(self.hidden_to_scores(new_hidden_size))
-        return q_values
-
-    def embed_observations(self, observations: str, description, inventory):
-        obs_idxs = []
-        for obs, descr, inventory in zip(observations, description, inventory):
-            # TODO: change sep to smth other?
-            state_description = (
-                "[CLS] " + "[SEP]".join([obs, descr, inventory]) + " [SEP]"
-            )
-
-            tokenized_state_description = self.tokenizer.tokenize(state_description)
-            cleaned_tokenized_state_decription = [
-                token
-                for token in tokenized_state_description
-                if token not in {"$", "|", "", "_", "\\", "/"}
-            ]
-
-            # BERT does not support sentences longer than 512 tokens
-            indexed_state_description = self.tokenizer.convert_tokens_to_ids(
-                cleaned_tokenized_state_decription[:512]
-            )
-            indexed_state_description = torch.tensor(
-                indexed_state_description, device=self.device
-            )
-            obs_idxs.append(indexed_state_description)
-        padded_idxs = pad_sequence(obs_idxs, batch_first=True)
-        _, state_repr = self.bert(padded_idxs)
-        return state_repr
-
-    def embed_actions(self, actions):
-        embedded_actions = []
-        # with torch.no_grad():
-        for action in actions:
-            tokenzed_action = self.tokenizer.tokenize(f"[CLS] {action} [SEP]")
-            action_indices = torch.tensor(
-                [self.tokenizer.convert_tokens_to_ids(tokenzed_action)],
-                device=self.device,
-            )
-            _, action_embedding = self.bert(action_indices)
-            embedded_actions.append(action_embedding)
-        return torch.cat(embedded_actions)
+def idx_select(collection: List, indices: List, reversed_indices=False) -> List:
+    """
+    performs fancy indexing
+    """
+    assert indices
+    if isinstance(indices[0], bool):
+        if reversed_indices:
+            indices = [not idx for idx in indices]
+        return [collection[i] for i, idx in enumerate(indices) if idx]
+    return [collection[idx] for idx in indices]
 
 
 class BaseQlearningAgent:
@@ -122,20 +46,23 @@ class BaseQlearningAgent:
         self,
         params: Params,
         net: QNet,
-        eps_scheduler_params,
+        eps_scheduler,
         experience_replay_buffer: Optional[Queue] = None,
     ) -> None:
         self._initialized = False
         self._episode_has_started = False
         self.max_steps_per_episode = params.pop("max_steps_per_episode")
+        self.batch_size = params.get("n_parallel_envs")
         self.experience_replay_buffer = experience_replay_buffer
         self.net = net
-        self.eps_scheduler = EpsScheduler(eps_scheduler_params)
+        self.eps_scheduler = eps_scheduler
         self.current_step = 0
+        self.gamefile = None
+        self.visited_states = defaultdict(set)
         self.training = False
         self.prev_actions = None
         self.prev_states = None
-        self.already_dones = None
+        self.already_dones = [False for _ in range(self.batch_size)]
 
     def train(self) -> None:
         """ Tell the agent it is in training mode. """
@@ -239,7 +166,8 @@ class BaseQlearningAgent:
         self._episode_has_started = False
         self.prev_actions = None
         self.prev_states = None
-        self.already_dones = None
+        self.already_dones = [False for _ in range(self.batch_size)]
+        self.visited_states = defaultdict(set)
         # [You can insert code here.]
 
     def reset(self):
@@ -252,31 +180,43 @@ class BaseQlearningAgent:
         dones: List[bool],
         infos: Dict[str, List[Any]],
     ):
-        batch_admissible_commands = infos["admissible_commands"]
-        states = [
-            State(description=description, feedback=obs, inventory=inventory)
-            for description, obs, inventory in zip(
-                infos["description"], observations, infos["inventory"]
-            )
-        ]
+        actions = ["pass" for _ in range(len(observations))]
+        not_done_idxs = idx_select(
+            list(range(self.batch_size)), self.already_dones, reversed_indices=True
+        )
+        self.gamefile = infos["gamefile"]
+        batch_admissible_commands = idx_select(
+            infos["admissible_commands"], not_done_idxs
+        )
+        states = idx_select(
+            [
+                State(description=description, feedback=obs, inventory=inventory)
+                for description, obs, inventory in zip(
+                    infos["description"], observations, infos["inventory"]
+                )
+            ],
+            not_done_idxs,
+        )
         # TODO: hzhz
-        # if random.random() < self.eps_scheduler.eps:
-        #     actions = [random.choice(adm_com) for adm_com in batch_admissible_commands]
-        # else:
-        #     self.net.eval()
-        #     q_values = self.net(states, batch_admissible_commands)
-        #     selected_idxs = [q_val.argmax().item() for q_val in q_values]
-        #
-        self.net.eval()
-        q_values = self.net(states, batch_admissible_commands)
-        selected_idxs = [softmax(q_val).multinomial(1).item() for q_val in q_values]
-        actions = [
-                    acts[idxs]
-                    for acts, idxs in zip(batch_admissible_commands, selected_idxs)
-                ]
+        if random.random() < self.eps_scheduler.eps:
+            selected_action_idxs = [
+                random.choice(len(adm_com)) for adm_com in batch_admissible_commands
+            ]
+        else:
+            self.net.eval()
+            q_values = self.net(states, batch_admissible_commands)
+            selected_action_idxs = [q_val.argmax().item() for q_val in q_values]
+
+        # self.net.eval()
+        # q_values = self.net(states, batch_admissible_commands)
+        # selected_action_idxs = [softmax(q_val, dim=1).multinomial(1).item() for q_val in q_values]
+        for not_done_idx, adm_com, sel_act_idx in zip(
+            not_done_idxs, batch_admissible_commands, selected_action_idxs
+        ):
+            actions[not_done_idx] = adm_com[sel_act_idx]
         self.update_experience_replay_buffer(
             next_states=states,
-            actions=actions,
+            actions=[a for a in actions if a != "pass"],
             batch_admissible_commands=batch_admissible_commands,
             rewards=rewards,
             dones=dones,
@@ -288,6 +228,7 @@ class BaseQlearningAgent:
     def update_experience_replay_buffer(
         self, next_states, actions, batch_admissible_commands, rewards, dones, is_lost
     ):
+        assert len(actions) == len(next_states)
         if self.prev_actions:
             for (
                 previous_state,
@@ -296,7 +237,6 @@ class BaseQlearningAgent:
                 admissible_commands,
                 reward,
                 done,
-                already_done,
                 game_lost,
             ) in zip(
                 self.prev_states,
@@ -305,27 +245,36 @@ class BaseQlearningAgent:
                 batch_admissible_commands,
                 rewards,
                 dones,
-                self.already_dones,
                 is_lost,
             ):
-                if not already_done:
-                    reward = float(reward)
-                    if done:
-                        if game_lost:
-                            reward = -1.0
-                        else:
-                            reward = 2.0
-                    self.experience_replay_buffer.put(
-                        Transition(
-                            previous_state=previous_state,
-                            next_state=next_state,
-                            action=action,
-                            allowed_actions=admissible_commands,
-                            reward=reward,
-                            done=done,
-                        )
+                assert action != "pass"
+                desc_inventory = next_state.description + next_state.inventory
+                reward, exploration_bonus = self.calculate_rewards(
+                    reward=reward, game_lost=game_lost, done=done, state=desc_inventory
+                )
+                self.visited_states[self.gamefile].add(desc_inventory)
+                self.experience_replay_buffer.put(
+                    Transition(
+                        previous_state=previous_state,
+                        next_state=next_state,
+                        action=action,
+                        allowed_actions=admissible_commands,
+                        exploration_bonus=exploration_bonus,
+                        reward=reward,
+                        done=done,
                     )
+                )
 
         self.prev_actions = actions
         self.prev_states = next_states
         self.already_dones = dones
+
+    def calculate_rewards(self, reward: int, game_lost: bool, done: bool, state: str):
+        reward = float(reward)
+        if done:
+            if game_lost:
+                reward = -2.0
+            else:
+                reward = 2.0
+        exploration_bonus = float(state in self.visited_states[self.gamefile])
+        return reward, exploration_bonus
