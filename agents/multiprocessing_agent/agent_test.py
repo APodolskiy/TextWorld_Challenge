044af338c8b3f60dfb83e6e002911d0405a69a03
Dima Lipin <dimich3d@ya.ru>
2019-03-20 13:58:30 +0300

    fixes for test script

--- agents/multiprocessing_agent/agent_test.py
+++ agents/multiprocessing_agent/agent_test.py
@@ -14,37 +14,28 @@ from agents.multiprocessing_agent.simple_net import SimpleNet
 from agents.multiprocessing_agent.utils import clean_text
 from agents.utils.eps_scheduler import EpsScheduler, DeterministicEpsScheduler
 
-required_infos = EnvInfos(
-    description=True,
-    inventory=True,
-    extras=["recipe", "walkthrough"],
-    admissible_commands=True,
-)
-
 
 def check_agent(game_file, train_params, agent_net: QNet):
     env_id = textworld.gym.register_games(
-        [game_file], required_infos, max_episode_steps=1000, name="check_agent"
+        [game_file],
+        BaseQlearningAgent.select_additional_infos(),
+        max_episode_steps=1000,
+        name="check_agent",
     )
     env_id = textworld.gym.make_batch(env_id, batch_size=1, parallel=False)
     env = gym.make(env_id)
     obs, infos = env.reset()
-    rewards = [0, ]
+    rewards = [0]
     dones = [False]
 
     actor = BaseQlearningAgent(
-        net=agent_net,
-        params=train_params,
-        eps_scheduler=DeterministicEpsScheduler(),
+        net=agent_net, params=train_params, eps_scheduler=DeterministicEpsScheduler()
     )
 
     print(infos["extra.walkthrough"])
 
     cnt = 0
     while not all(dones) and cnt < 10:
-        infos["gamefile"] = game_file[0]
-        infos["is_lost"] = [False]
-
         commands = actor.act(obs, rewards, dones, infos)
         print(f">{commands[0]}")
         obs, cumulative_rewards, dones, infos = env.step(commands)
@@ -60,4 +51,6 @@ if __name__ == "__main__":
     agent = SimpleNet(device="cpu", tokenizer=spacy.load("en_core_web_sm").tokenizer)
     agent.load_state_dict(torch.load(params["training"]["model_path"]))
     game_file = f"games/train_sample/{args.game_file}"
-    check_agent(game_file=game_file, agent_net=agent, train_params=params.pop("training"))
+    check_agent(
+        game_file=game_file, agent_net=agent, train_params=params.pop("training")
+    )
