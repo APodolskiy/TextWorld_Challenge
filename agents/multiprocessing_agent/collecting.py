72fd0e39f5e23aacc007c672c7b706dca7139734
Dima Lipin <dimich3d@ya.ru>
2019-03-20 13:59:17 +0300

    use cum. rewards, don't recreate env

--- agents/multiprocessing_agent/collecting.py
+++ agents/multiprocessing_agent/collecting.py
@@ -4,12 +4,10 @@ import torch
 from logging import info
 
 import gym
+import textworld.gym
 import numpy
-import textworld
 from tqdm import tqdm
 from agents.multiprocessing_agent.custom_agent import BaseQlearningAgent
-from agents.multiprocessing_agent.bert_net import QNet
-from test_submission import _validate_requested_infos
 from tensorboardX import SummaryWriter
 
 collecting_step = 1
@@ -20,33 +18,31 @@ def collect_experience(
     buffer: Queue,
     train_params,
     eps_scheduler,
-    target_net: QNet,
-    policy_net: QNet,
+    target_net,
+    policy_net,
     log_dir,
+    env=None,
 ):
     global collecting_step
     if log_dir is not None:
         writer = SummaryWriter(log_dir)
     actor = BaseQlearningAgent(
-        net=policy_net,
-        params=train_params,
-        eps_scheduler=eps_scheduler,
-    )
-    requested_infos = actor.select_additional_infos()
-
-    env_id = textworld.gym.register_games(
-        game_files,
-        requested_infos,
-        max_episode_steps=actor.max_steps_per_episode,
-        name="training_par",
+        net=policy_net, params=train_params, eps_scheduler=eps_scheduler
     )
     batch_size = train_params.pop("n_parallel_envs")
-    env_id = textworld.gym.make_batch(
-        env_id,
-        batch_size=batch_size,
-        parallel=train_params.pop("use_separate_process_envs"),
-    )
-    env = gym.make(env_id)
+    if env is None:
+        env_id = textworld.gym.register_games(
+            game_files,
+            BaseQlearningAgent.select_additional_infos(),
+            max_episode_steps=actor.max_steps_per_episode,
+            name="training_par",
+        )
+        env_id = textworld.gym.make_batch(
+            env_id,
+            batch_size=batch_size,
+            parallel=train_params.pop("use_separate_process_envs"),
+        )
+        env = gym.make(env_id)
 
     update_freq = train_params.pop("target_net_update_freq")
     for epoch_no in range(1, train_params.pop("n_epochs_collection") + 1):
@@ -56,41 +52,23 @@ def collect_experience(
                 target_net.load_state_dict(policy_net.state_dict())
 
             obs, infos = env.reset()
-            # print(infos["extra.walkthrough"][0])
             actor.train()
 
             dones = [False for _ in range(batch_size)]
             steps = [0 for _ in range(batch_size)]
-            cumulative_rewards = None
-            prev_cumulative_rewards = [0 for _ in range(batch_size)]
-            rewards = prev_cumulative_rewards
+            cumulative_rewards = [0 for _ in range(batch_size)]
 
             while not all(dones):
                 steps = [step + int(not done) for step, done in zip(steps, dones)]
 
-                infos["is_lost"] = [
-                    ("You lost!" in o if d else False) for o, d in zip(obs, dones)
-                ]
                 # TODO: only one game is supported
                 infos["gamefile"] = game_files[0]
                 with torch.no_grad():
-                    command = actor.act(obs, rewards, dones, infos)
-                # print(obs[0])
-                # print(infos["description"][0])
-                # print(command[0])
-                # print("*" * 50)
+                    command = actor.act(obs, cumulative_rewards, dones, infos)
                 obs, cumulative_rewards, dones, infos = env.step(command)
-
-                rewards = [
-                    c_r - p_c_r
-                    for c_r, p_c_r in zip(cumulative_rewards, prev_cumulative_rewards)
-                ]
                 prev_cumulative_rewards = cumulative_rewards
-            infos["is_lost"] = [
-                ("You lost!" in o if d else False) for o, d in zip(obs, dones)
-            ]
             infos["gamefile"] = game_files[0]
-            actor.act(obs, rewards, dones, infos)
+            actor.act(obs, cumulative_rewards, dones, infos)
             assert all([actor.history[i][-1].done for i in range(actor.batch_size)])
             for game in actor.history.values():
                 buffer.put(game)
