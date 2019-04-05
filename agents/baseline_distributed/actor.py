1a5439c46f128a4062cbd35df538c8722df80288
APodolskiy <originalea@yandex.ru>
2019-04-05 15:40:10 +0300

    Continue development of distributed agent

--- agents/baseline_distributed/actor.py
+++ agents/baseline_distributed/actor.py
@@ -1,52 +1,89 @@
-from typing import List
-
+import gym
 import spacy
 import textworld.gym
-
 import torch
 import torch.multiprocessing as mp
-import torch.optim as optim
-
+from tqdm import tqdm
 from agents.baseline_distributed.model import LSTM_DQN
+from agents.baseline_distributed.utils.data_utils import preprocess
 from agents.utils.eps_scheduler import LinearScheduler
-from agents.utils.replay import TernaryPrioritizeReplayMemory
-
-
 class Actor(mp.Process):
-    def __init__(self, id: int, eps, request_infos, config, shared_state, shared_replay_memory, shared_writer):
+    def __init__(self,
+                 actor_id: int,
+                 eps,
+                 request_infos,
+                 game_files,
+                 config,
+                 shared_state,
+                 shared_replay_memory,
+                 shared_writer):
         super(Actor, self).__init__()
         self.word_vocab = []
         self._load_vocab(vocab_file="./vocab.txt")
         self.EOS_id = self.word2id["</S>"]
         self.SEP_id = self.word2id["SEP"]
-
         self.writer = shared_writer
-
+        self.game_files = game_files
+        self.id = actor_id
         self.request_infos = request_infos
+        self.shared_state = shared_state
+        self.shared_replay_memory = shared_replay_memory
         self.config = config
         self.nb_epochs = self.config['training']['nb_epochs']
         self.batch_size = self.config['training']['batch_size']
         self.max_nb_steps_per_episode = self.config['training']['max_nb_steps_per_episode']
-
         self.use_cuda = True and torch.cuda.is_available()
         self.model = LSTM_DQN(config=self.config["model"], word_vocab=self.word_vocab)
-
         self.nlp = spacy.load('en', disable=['ner', 'parser', 'tagger'])
-
         self.eps = eps
         self.eps_scheduler = LinearScheduler(self.config["exploration"])
         self.act_steps = 0
         self.episode_steps = 0
         self.num_episodes = 0
-
+        self.NUM_EPOCHS = 10000
         self._episode_started = False
         self.previous_actions: List[str] = []
         self.scores: List[List[int]] = []
         self.dones: List[List[int]] = []
         self.prev_description_id: List = None
         self.prev_command: List = None
-
-    def run(self, game_files):
-        env_id = textworld.gym.register_games(game_files, self.request_infos, max_episode_steps=200, name='training')
+    def run(self):
+        env_id = textworld.gym.register_games(self.game_files,
+                                              self.request_infos,
+                                              max_episode_steps=200,
+                                              name='training')
         env_id = textworld.gym.make_batch(env_id, batch_size=1, parallel=False)
-
+        env = gym.make(env_id)
+        for epoch_no in range(self.NUM_EPOCHS):
+            stats = {
+                "scores": [],
+                "steps": []
+            }
+            steps = 0
+            for _ in tqdm(range(len(self.game_files))):
+                obs, infos = env.reset()
+                done, score = False, 0
+                while not done:
+                    action = self.act(obs, infos)
+                    obs, score, done, infos = env.step(action)
+                    steps += 1
+                stats["scores"].append(score)
+                stats["steps"].append(steps)
+    def act(self, obs: str, infos: Dict[str, List[Any]]) -> str:
+        pass
+    def get_game_state_info(self, obs: str, infos: Dict[str, List[Any]]) -> torch.Tensor:
+        description_tokens = preprocess(infos["description"], "description", tokenizer=self.nlp)
+        if len(description_tokens) == 0:
+            description_tokens = ["end"]
+        description_ids = None
+        res = 0
+        inventory_tokens = preprocess(infos["inventory"], "inventory", tokenizer=self.nlp)
+        inventory_ids = None
+        recipe_tokens = preprocess(infos["extra.recipe"], "recipe", tokenizer=self.nlp)
+        recipe_ids = None
+        feedback_tokens = preprocess(infos["feedback"], "feedback", tokenizer=self.nlp)
+        feedback_ids = None
+        state_ids = description_ids + inventory_ids + recipe_ids
+        input_description = pad_sequences(state_id_list, maxlen=max_len(state_id_list)).astype('int32')
+        input_description = to_pt(input_description, self.use_cuda)
+        return input_description
---
+++ agents/baseline_distributed/data_utils/preprocess.py
@@ -0,0 +1,86 @@
+from typing import Optional
+INFO_TYPES = ["description", "feedback", "inventory", "recipe"]
+def preprocess(text: Optional[str], info_type, tokenizer=None, lower_case=True):
+    assert info_type in INFO_TYPES
+    text = clean_text(text, info_type)
+def clean_text(text: str, text_type: str) -> str:
+    assert type in INFO_TYPES
+    if text_type == "feedback":
+        if "$$$$$$$" in text:
+            text = ""
+        if "-=" in text:
+            text = text.split("-=")[0]
+    elif text_type == "inventory":
+        text = [it.strip() for it in text.split("\n") if len(it) > 0]
+        if len(text) > 0:
+            pass
+    elif text_type == "recipe":
+        pass
+    else:
+        pass
+    return text
+def tokenize_text(text: str, text_type: str):
+    pass
+def clean_text(s, type_):
+    assert type_ in ["inventory", "feedback", "description"]
+    if type_ == "inventory":
+        result = s.split("carrying")[1]
+        if result[0] == ":":
+            result = result[1:]
+    elif type_ == "feedback":
+        result = s
+        if "$$$$$$$" in result:
+            result = ""
+        if "-=" in result:
+            result = result.split("-=")[0]
+    else:
+        result = s
+    result = result.strip()
+    if not result:
+        result = "nothing"
+    return result
+def preproc(s, str_type='None', tokenizer=None, lower_case=True):
+    if s is None:
+        return ["nothing "]
+    s = s.replace("\n", ' ')
+    if s.strip() == "":
+        return ["nothing"]
+    if str_type == 'feedback':
+        if "$$$$$$$" in s:
+            s = ""
+        if "-=" in s:
+            s = s.split("-=")[0]
+    s = s.strip()
+    if len(s) == 0:
+        return ["nothing"]
+    tokens = [t.text for t in tokenizer(s)]
+    if lower_case:
+        tokens = [t.lower() for t in tokens]
+    return tokens
+if __name__ == "__main__":
+    feedback = "You are hungry! Let's cook a delicious meal. Check the cookbook in the kitchen\n" \
+                "for the recipe. Once done, enjoy your meal!\n" \
+                "\n" \
+                "-= Kitchen =-\n" \
+                "Guess what, you are in a place we're calling a kitchen.\n" \
+                "\n" \
+                "You make out a fridge. The fridge contains a block of cheese. You can make out\n" \
+                "an oven. You make out a table. The table is massive. But the thing is empty. You\n" \
+                "see a counter. The counter is vast. On the counter you can see a cookbook and a\n" \
+                "knife. You can make out a stove. But there isn't a thing on it."
+    print(clean_text(feedback, "feedback"))
+    inventory = "You are carrying:\n" \
+                "  a roasted white onion\n" \
+                "  a red onion\n"
+    print(inventory)
+    print(clean_text(inventory, "inventory"))
+    description = "-= Kitchen =-\n" \
+                  "Guess what, you are in a place we're calling a kitchen.\n" \
+                  "\n" \
+                  "You make out a fridge. The fridge contains a block of cheese. You can make out\n" \
+                  "an oven. You make out a table. The table is massive. But the thing is empty. You\n" \
+                  "see a counter. The counter is vast. On the counter you can see a cookbook and a\n" \
+                  "knife. You can make out a stove. But there isn't a thing on it.\n"
+    print(clean_text(description, "description"))
+    inventory = [it.strip() for it in inventory.split("\n") if len(it) > 0]
+    print(inventory)
