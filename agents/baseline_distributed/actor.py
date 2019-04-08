c26855c3b55de2b3824e6989587169fdc902e68e
APodolskiy <originalea@yandex.ru>
2019-04-08 17:55:25 +0300

    Add game state description extraction.

--- agents/baseline_distributed/actor.py
+++ agents/baseline_distributed/actor.py
@@ -1,7 +1,11 @@
+import json
+from pathlib import Path
 from typing import List, Dict, Any
 
+from _jsonnet import evaluate_file
 import gym
 import spacy
+from textworld import EnvInfos
 import textworld.gym
 from tqdm import tqdm
 
@@ -9,8 +13,9 @@ import torch
 import torch.multiprocessing as mp
 
 from agents.baseline_distributed.model import LSTM_DQN
-from agents.utils.eps_scheduler import LinearScheduler
 from agents.baseline_distributed.data_utils.preprocess import preprocess, SEP_TOKEN, ITM_TOKEN
+from agents.utils.eps_scheduler import LinearScheduler
+from agents.utils.generic import _words_to_ids
 
 
 class Actor(mp.Process):
@@ -25,8 +30,9 @@ class Actor(mp.Process):
                  shared_writer):
         super(Actor, self).__init__()
         self.word_vocab = []
+        self.word2id = {}
         self._load_vocab(vocab_file="./vocab.txt")
-        self.EOS_id = self.word2id["</S>"]
+        self.EOS_id = self.word2id["</s>"]
         self.SEP_id = self.word2id[SEP_TOKEN]
         self.ITM_id = self.word2id[ITM_TOKEN]
 
@@ -78,31 +84,58 @@ class Actor(mp.Process):
                 obs, infos = env.reset()
                 done, score = False, 0
                 while not done:
-                    action = self.act(obs, infos)
+                    action = self.act(obs[0], infos, score, done)
                     obs, score, done, infos = env.step(action)
                     steps += 1
                 stats["scores"].append(score)
                 stats["steps"].append(steps)
 
-    def act(self, obs: str, infos: Dict[str, List[Any]]) -> str:
+    def act(self, obs: str, infos: Dict[str, List[Any]], score: int, done: bool) -> str:
+        state_description = self.get_game_state_info(obs, infos)
         pass
 
-    def get_game_state_info(self, obs: str, infos: Dict[str, List[Any]]) -> torch.Tensor:
-        description_tokens = preprocess(infos["description"], "description", tokenizer=self.nlp)
+    def get_game_state_info(self, obs: str, infos: Dict[str, Any]) -> torch.Tensor:
+        description_tokens = preprocess(infos["description"][0], "description", tokenizer=self.nlp)
         if len(description_tokens) == 0:
             description_tokens = ["end"]
-        description_ids = None
+        description_ids = _words_to_ids(description_tokens, self.word2id)
 
-        inventory_tokens = preprocess(infos["inventory"], "inventory", tokenizer=self.nlp)
-        inventory_ids = None
+        inventory_tokens = preprocess(infos["inventory"][0], "inventory", tokenizer=self.nlp)
+        inventory_ids = _words_to_ids(inventory_tokens, self.word2id)
 
-        recipe_tokens = preprocess(infos["extra.recipe"], "recipe", tokenizer=self.nlp)
-        recipe_ids = None
+        recipe_tokens = preprocess(infos["extra.recipe"][0], "recipe", tokenizer=self.nlp)
+        recipe_ids = _words_to_ids(recipe_tokens, self.word2id)
 
-        feedback_tokens = preprocess(infos["feedback"], "feedback", tokenizer=self.nlp)
-        feedback_ids = None
+        feedback_tokens = preprocess(obs, "feedback", tokenizer=self.nlp)
+        feedback_ids = _words_to_ids(feedback_tokens, self.word2id)
 
-        state_ids = description_ids + inventory_ids + recipe_ids
-        input_description = pad_sequences(state_ids, maxlen=max_len(state_ids)).astype('int32')
-        input_description = to_pt(input_description, self.use_cuda)
+        state_ids = description_ids + [self.SEP_id] + inventory_ids + [self.SEP_id] + recipe_ids + [self.EOS_id]
+        input_description = torch.tensor(state_ids, dtype=torch.long)
         return input_description
+
+    # TODO: replace load_vocab method with the argument in constructor
+    def _load_vocab(self, vocab_file: str) -> None:
+        with open(vocab_file, "r") as fp:
+            self.word_vocab = fp.read().split("\n")
+        for i, w in enumerate(self.word_vocab + [SEP_TOKEN] + [ITM_TOKEN]):
+            self.word2id[w] = i
+
+
+if __name__ == '__main__':
+    train_dir = Path("games/dima_sosat_game")
+    games = [str(f) for f in train_dir.iterdir() if f.is_file() and f.suffix == '.ulx']
+
+    def select_additional_infos():
+        request_infos = EnvInfos()
+        request_infos.description = True
+        request_infos.inventory = True
+        request_infos.entities, request_infos.verbs = True, True
+        request_infos.max_score = True
+        request_infos.extras = ["recipe"]
+        request_infos.admissible_commands = True
+        return request_infos
+
+    actor = Actor(actor_id=0, eps=0.3, request_infos=select_additional_infos(), game_files=games,
+                  config=json.loads(evaluate_file("configs/dqn_config.jsonnet")), shared_state=None,
+                  shared_replay_memory=None, shared_writer=None)
+    actor.run()
